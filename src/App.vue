e28b960a9fe19399b93b93d9bed6372d008be0cd
Yashakk <yashashree.kulkarni05@gmail.com>
2021-04-16 11:39:58 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import MessageComponent from './components/MessageComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MessageComponent
+  }
+}
+</script>
+
+<style>
+app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tests/unit/example.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+const MessageComponent = {
+  template: '<p>{{ msg }} </p>',
+  props: ['msg']
+}
+describe('MessageComponent', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(MessageComponent, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
